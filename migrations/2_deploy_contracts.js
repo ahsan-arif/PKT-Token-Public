789dad58b90e6460486099f8c6956b2dfe3e361d
ahsan-arif <ahsan.arifofficial@gmail.com>
2021-07-30 21:20:17 +0500

    added ropsten configuration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var PKTFactory = artifacts.require("./PKTFactory.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(PKTFactory);
+module.exports = function(deployer,network,accounts) {
+  await deployer.deploy(PKTFactory);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = "INFURA_KEY_PLACED;"
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +13,17 @@ module.exports = {
       host : "127.0.0.1",
       port: 7545,
       network_id : "*"
+    },
+    ropsten :{
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/d727f2046b3c42e3931fd0ac2b8e2184")
+      },
+      network_id: 3,
+      networkCheckTimeout: 10000000,
+      confirmations: 2,
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true ,
+      gas: 5500000      //make sure this gas allocation isn't over 4M, which is the max
     }
   },
   contracts_directory: './contracts/',
